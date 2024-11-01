4edf518c1dae8fef6b3cd25457df789c676f457a
Kyla Camille Ocampo <kylacamilleocamp@onyx.boisestate.edu>
2024-11-01 17:10:10 -0600

    Adding item works but table is wonky

--- js/main.js
+++ js/main.js
@@ -2,25 +2,24 @@ window.onload = loaded;
 
 let lambdaUrl = "https://m14zlk7u19.execute-api.us-east-2.amazonaws.com/items"
 
-function sendData() {
-    // document.getElementById("send-data").onclick = function () {
-        let xhr = new XMLHttpRequest();
-        xhr.open("PUT", lambdaUrl);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({
-            "id": "1111",
-            "price": 19,
-            "name": "test put value"
-        }));
-}
-
 function loadData(){
     let lambda = document.getElementById("myTable");
     let xhr = new XMLHttpRequest();
     xhr.addEventListener("load", function () {
         // lambda.innerHTML = xhr.response;
+        lambda.innerHTML = ""; // no duplicates
         const items = JSON.parse(xhr.response);
 
+        var title = lambda.insertRow();
+        var idTitle = title.insertCell(0);
+        var nameTitle = title.insertCell(1);
+        var priceTitle = title.insertCell(2);
+        // var idTitle = title.insertCell(3);
+
+        idTitle.innerText = "ID";
+        nameTitle.innerText = "name";
+        priceTitle.innerText = "price";
+        
         items.forEach(item => {
             //cell.appendChild(cellText);
             //row.appendChild(cell);
@@ -51,5 +50,29 @@ function deleteData() {
         xhr.open("DELETE", "https://m14zlk7u19.execute-api.us-east-2.amazonaws.com/items/1111");
         xhr.setRequestHeader("Content-Type", "application/json");
         xhr.send();
+    
+}
+function sendData() {
+    // document.getElementById("send-data").onclick = function () {
+        let xhr = new XMLHttpRequest();
+        xhr.open("PUT", "https://m14zlk7u19.execute-api.us-east-2.amazonaws.com/items");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        var id = document.getElementById("id");
+        var name = document.getElementById("name");
+        var price = document.getElementById("price");
+        var check = document.getElementById("checkItemAdded");
+
+        if (id.value != "" || name.value != "" || price.value != "") {
+        xhr.send(JSON.stringify({
+                    "id": id.value,
+                    "price": price.value,
+                    "name": name.value
+                }));
+                check.textContent = "Added Item. Load table to check";
+        } else {
+            check.textContent = "Cannot add empty values";
+        }
+        
     
 }
